d2e782e09bbeda7a1e47fdf86972775a15850cc4
Joaquin Cesar <joaquincesar777@gmail.com>
2022-08-21 18:27:58 -0300

    NavBar

---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,23 @@
+import logo from '../../logo.svg';
+import "./styles.css";
+
+const NavBar = () =>{
+    return(
+    
+        <div className='NavBar-container'>
+          <div> <img src={logo} width={"50px"} alt= "logo"/></div>  
+          <div>
+            <ul>
+              <li><a href='#'>Todos</a></li>
+              <li><a href='#'>Hamburguesa</a></li>
+              <li><a href='#'>Pizza</a></li>
+              <li><a href='#'>Complementos</a></li>
+              <li><a href='#'>Bebida</a></li>
+              <li><a href='#'>Postre</a></li>
+              </ul>
+           </div> 
+          <div><button>Login</button></div> 
+        </div>
+    )
+}
+export default NavBar
